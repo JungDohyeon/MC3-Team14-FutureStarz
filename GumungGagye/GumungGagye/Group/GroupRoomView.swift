42bf6e539f63f09c6f39ebfd419ff5ccf3400ec7
JungDohyeon <jdh1109ok@naver.com>
2023-07-10 10:04:07 +0900

    [FEAT] #21 중간커밋

--- GumungGagye/GumungGagye/Group/GroupRoomView.swift
+++ GumungGagye/GumungGagye/Group/GroupRoomView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct GroupRoomView: View {
     
     let groupdata: GroupData
+    var isNotExist: Bool
     
     var body: some View {
         ZStack {
@@ -23,8 +24,14 @@ struct GroupRoomView: View {
                     
                     Spacer()
                     
-                    Rectangle()
-                        .frame(width:24, height: 24)
+                    if groupdata.lock_status {
+                        Image(systemName: "lock.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18)
+                            .foregroundColor(Color("Gray2"))
+                    }
+                    
                 }
                 .padding(.bottom, 8)
                 
@@ -38,24 +45,29 @@ struct GroupRoomView: View {
                 }
                 .padding(.bottom, 16)
                 
-                HStack(spacing: 0) {
+                HStack(spacing: 23) {
                     HStack(spacing: 0) {
-                        Rectangle()
-                            .frame(width:24, height: 24)
-                            .padding(.trailing, 8)
+                        Image(systemName: "person.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18, height: 19)
+                            .foregroundColor(Color("Gray2"))
+                            .padding(.trailing, 11)
                         
                         Text(groupdata.group_goal.description)
                             .modifier(Body2())
                             .foregroundColor(Color("Gray1"))
                         
                     }
-                    .padding(.trailing, 20)
                     
                     
                     HStack(spacing: 0) {
-                        Rectangle()
-                            .frame(width:24, height: 24)
-                            .padding(.trailing, 8)
+                        Image(systemName: "person.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18, height: 19)
+                            .foregroundColor(Color("Gray2"))
+                            .padding(.trailing, 11)
                         
                         Group {
                             Text(groupdata.group_cur.description)
@@ -70,17 +82,20 @@ struct GroupRoomView: View {
                     
                     Spacer()
                     
-                    NavigationLink {
-                        
-                    } label: {
-                       GroupSignInBtn()
+                    if isNotExist {
+                        NavigationLink {
+                            
+                        } label: {
+                           GroupSignInBtn()
+                        }
                     }
-
                 }
                 .padding(.bottom, 24)
                 
-                Divider()
-                    .background(Color("Gray3"))
+                if isNotExist {
+                    Divider()
+                        .background(Color("Gray3"))
+                }
             }
         }
     }
@@ -91,12 +106,13 @@ struct GroupSignInBtn: View {
     var body: some View {
         Text("가입하기")
             .modifier(BtnBold())
-            .foregroundColor(.white)
+            .foregroundColor(Color("Main"))
             .padding(.horizontal, 15)
             .padding(.vertical, 8)
             .background(
                 Rectangle()
-                    .foregroundColor(Color("Main"))
+                    .foregroundColor(Color("Light"))
+                    .cornerRadius(5)
             )
     }
 }
@@ -104,6 +120,6 @@ struct GroupSignInBtn: View {
 
 struct GroupRoomView_Previews: PreviewProvider {
     static var previews: some View {
-        GroupRoomView(groupdata: GroupData(id: "1", group_name: "test", group_introduce: "test", group_goal: 100000, group_cur: 7, group_max: 10, lock_status: true, group_pw: "1234"))
+        GroupRoomView(groupdata: GroupData(id: "1", group_name: "test", group_introduce: "test", group_goal: 100000, group_cur: 7, group_max: 10, lock_status: true, group_pw: "1234"), isNotExist: true)
     }
 }
