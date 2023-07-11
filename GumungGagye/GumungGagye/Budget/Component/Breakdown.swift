344a86cc4491047f835ba9e793ee5bb99f149932
juwonleee <juwonlee0509@gmail.com>
2023-07-11 10:55:39 +0900

    [FEAT] #19 Add Componens

---
+++ GumungGagye/GumungGagye/Budget/Component/Breakdown.swift
@@ -0,0 +1,20 @@
+//
+//  Breakdown.swift
+//  GumungGagye
+//
+//  Created by Lee Juwon on 2023/07/11.
+//
+
+import SwiftUI
+
+struct Breakdown: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Breakdown_Previews: PreviewProvider {
+    static var previews: some View {
+        Breakdown()
+    }
+}
---
+++ GumungGagye/GumungGagye/Budget/Component/DateBreakdown.swift
@@ -0,0 +1,20 @@
+//
+//  DateBreakdown.swift
+//  GumungGagye
+//
+//  Created by Lee Juwon on 2023/07/11.
+//
+
+import SwiftUI
+
+struct DateBreakdown: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateBreakdown_Previews: PreviewProvider {
+    static var previews: some View {
+        DateBreakdown()
+    }
+}
