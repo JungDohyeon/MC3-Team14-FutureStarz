31da471bc40ce8d93be30812394bbf620b9cacd6
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-07-14 20:58:47 +0900

    [Feat] Login Firebase

---
+++ GumungGagye/GumungGagye/Onboarding/Authentication/SettingsView.swift
@@ -0,0 +1,140 @@
+//
+//  SettingsView.swift
+//  GumungGagye
+//
+//  Created by 신상용 on 2023/07/14.
+//
+
+import SwiftUI
+
+@MainActor
+final class SettingsViewModel: ObservableObject {
+    
+    @Published var authProviders: [AuthProviderOption] = []
+    
+    func loadAuthProviders() {
+        if let providers = try? AuthenticationManager.shared.getProviders() {
+            authProviders = providers
+        }
+    }
+    
+    func signOut() throws {
+        try AuthenticationManager.shared.signOut()
+    }
+    
+    func deleteAccount() async throws {
+        try await AuthenticationManager.shared.delete()
+    }
+    
+    func resetPassword() async throws {
+        let authUser = try AuthenticationManager.shared.getAuthenticatedUser()
+        
+        guard let email = authUser.email else {
+            throw URLError(.fileDoesNotExist)
+        }
+        
+        try await AuthenticationManager.shared.resetPassword(email: email)
+    }
+    
+    func updateEmail() async throws {
+        let email = "hello123@gmail.com"
+        try await AuthenticationManager.shared.updateEmail(email: email)
+    }
+    
+    func updatePassword() async throws {
+        let password = "Hello123!"
+        try await AuthenticationManager.shared.updatePassword(password: password)
+    }
+}
+
+
+struct SettingsView: View {
+    
+    @StateObject private var viewModel = SettingsViewModel()
+    @Binding var showSignInView: Bool
+    var body: some View {
+        List {
+            Button("Log out") {
+                Task {
+                    do {
+                        try viewModel.signOut()
+                        showSignInView = true
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            Button(role: .destructive){
+                Task {
+                    do {
+                        try await viewModel.deleteAccount()
+                        showSignInView = true
+                    } catch {
+                        print(error)
+                    }
+                }
+            } label: {
+                Text("Delete account")
+            }
+            
+            if viewModel.authProviders.contains(.email) {
+                emailSection
+            }
+            
+        }
+        .onAppear {
+            viewModel.loadAuthProviders()
+        }
+        .navigationTitle("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            SettingsView(showSignInView: .constant(false))
+        }
+    }
+}
+
+extension SettingsView {
+    private var emailSection: some View {
+        Section {
+            Button("Reset Password") {
+                Task {
+                    do {
+                        try await viewModel.resetPassword()
+                        print("PASSWORD RESET")
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            Button("Update Password") {
+                Task {
+                    do {
+                        try await viewModel.updatePassword()
+                        print("PASSWORD UPDATED")
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            Button("Update Email") {
+                Task {
+                    do {
+                        try await viewModel.updateEmail()
+                        print("EMAIL UPDATED")
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+        } header: {
+            Text("Email function")
+        }
+    }
+}
