43b24a2f50a72d6f548b9c725f97915b4a5f17c1
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-07-14 20:58:47 +0900

    [Feat] Login Firebase

---
+++ GumungGagye/GumungGagye/Onboarding/SeletingName.swift
@@ -0,0 +1,20 @@
+//
+//  SeletingName.swift
+//  GumungGagye
+//
+//  Created by 신상용 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct SeletingName: View {
+    var body: some View {
+        Text("onboarding_2")
+    }
+}
+
+struct SeletingName_Previews: PreviewProvider {
+    static var previews: some View {
+        SeletingName()
+    }
+}
