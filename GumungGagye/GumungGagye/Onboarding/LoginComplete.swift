53ce9de754d484b2908e8c905febdb1ed232f07a
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-07-15 21:40:35 +0900

    [Feat] - 뷰 그리기

---
+++ GumungGagye/GumungGagye/Onboarding/LoginComplete.swift
@@ -0,0 +1,23 @@
+//
+//  LoginComplete.swift
+//  GumungGagye
+//
+//  Created by 신상용 on 2023/07/15.
+//
+
+import SwiftUI
+
+struct LoginComplete: View {
+    var body: some View {
+        VStack {
+            Text("Login Complete")
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct LoginComplete_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginComplete()
+    }
+}
