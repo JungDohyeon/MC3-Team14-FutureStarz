a60fa1371a99016c54c77c1169b9af9d18a64c40
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-07-29 23:31:45 +0900

    [Fix] #102 OnboardingView, LaunchScreen, AppIcon

--- GumungGagye/GumungGagye/Onboarding/BankCardPayView.swift
+++ GumungGagye/GumungGagye/Onboarding/BankCardPayView.swift
@@ -15,12 +15,13 @@ struct BankCardPayView: View {
     @Binding var selectBankCardPayIndex: Int
     @Binding var isAbled: Bool
     let inputdata = InputUserData.shared
-//    var card_info = [[0:"선택안함"],[1: "신한은행", 2: "국민은행", 3: "하나은행", 4: "우리은행", 5: "농협은행", 6: "기업은행", 7: "제일은행", 8: "카카오뱅크", 9: "토스"], [1:"카카오페이", 2:"네이버페이", 3: "페이코"], [1:"신한카드", 2:"현대카드", 3:"삼성카드", 4:"롯데카드", 5:"국민카드", 6:"하나카드", 7:"우리카드", 8:"농협카드"]]
+
     let bankCardPayData = BankCardPay.shared
     
     var body: some View {
         VStack(spacing: 0) {
-            Image("bank\(index)")
+//            Image("bank\(index)")
+            Image(bankCardPayData.card_info[bankCardPay][index]![0])
                 .frame(width: 32, height: 32)
                 .padding(.bottom, 4)
             Text(bankCardPayData.card_info[bankCardPay][index]![0])
@@ -51,7 +52,7 @@ struct BankCardPayView: View {
 
 struct BankCardPayView_Previews: PreviewProvider {
     static var previews: some View {
-        BankCardPayView(bankCardPay: 1, index: 9, selectBankCardPay: .constant(1), selectBankCardPayIndex: .constant(1), isAbled: .constant(false))
+        BankCardPayView(bankCardPay: 1, index: 1, selectBankCardPay: .constant(1), selectBankCardPayIndex: .constant(1), isAbled: .constant(false))
             .previewLayout(.fixed(width: 108, height: 80))
     }
 }
